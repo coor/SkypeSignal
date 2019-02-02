0c044c8c9d5ed64efd7ff0a7a842de66a4d24766
hannes <hannes@tulpenweg.info>
2019-02-03 00:25:43 +0100

    enable bt on ESP32S

--- src/Arduino/SkypeSignal/SkypeSignal.ino
+++ src/Arduino/SkypeSignal/SkypeSignal.ino
@@ -1,14 +1,14 @@
 #include <Adafruit_NeoPixel.h>
 
-/*
-Patrick Squire - Resdevops.com
-Copyright (C) 2016 - All Rights Reserved
-*/
+#include "BluetoothSerial.h" 
+
+#define ENABLE_SERIAL 1
+#define ENABLE_BT     1
 
 //Const:
-int dataPin = 6;  // Arduino PWM data pin D6
-int pixels = 1;  // number of Neopixels
-int SkypeCommand;
+int dataPin = 27;  // Arduino PWM data pin D6
+int pixels = 1;    // number of Neopixels
+int SkypeCommand = 0;
 int LoopCommand;
 
 // Parameter 1 = number of pixels in strip
@@ -18,28 +18,55 @@ int LoopCommand;
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixels, dataPin, NEO_GRB + NEO_KHZ800);
+
+#ifdef ENABLE_BT
+int BTCommand;
+BluetoothSerial ESP_BT; //Object for Bluetooth
+#endif
+
+Adafruit_NeoPixel strip=Adafruit_NeoPixel(pixels, dataPin, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-	//Create Serial Object
-	Serial.begin(9600);
+
+#ifdef ENABLE_SERIAL
+  Serial.begin(115200); //Start Serial monitor in 115200
+#endif  
+
+#ifdef ENABLE_BT
+  ESP_BT.begin("SkypeSignalBT"); //Name of your Bluetooth Signal
+#endif
+
+  pinMode (dataPin, OUTPUT);//Specify that LED pin is output
 
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.show();
 }
 
 void loop()
 {
-    //Read Input and assume that its a int type not a char
-  	SkypeCommand = Serial.read() - '0';
+
+#if ENABLE_SERIAL
+   //Read Input and assume that its a int type not a char
+   if (Serial.available()>0)
+   {
+     SkypeCommand = Serial.read() - '0';
+   }
+#endif
   
+#ifdef ENABLE_BT
+  if (ESP_BT.available()>0) //Check if we receive anything from Bluetooth
+  {
+    SkypeCommand = ESP_BT.read() - '0'; //Read what we recevive 
+  }
+#endif
+
    if (SkypeCommand > 0)
    {
     LoopCommand = SkypeCommand;
    }
   
-  	switch (LoopCommand)
+   switch (LoopCommand)
    {
     case 1:
       //Online [Green]
@@ -72,12 +99,10 @@ void loop()
     default:
       //LED's are off
       SetLedColour(0, 0, 0);  
-   }	
+   }  
   	//Echo the output
   	//Serial.println(LoopCommand);  
   
-   //Flush Serial Bus at end
-   Serial.flush();
 }
 
 void InACall()
@@ -85,46 +110,34 @@ void InACall()
     // Fade IN
     for(int k = 0; k < 255; k++) 
     { 
-       for (int i = 0; i < pixels; i++) 
-       {
-        strip.setPixelColor(i, 0, k, 0);
-       }
-         strip.show();
-        delay(4);     
+       SetLedColour(0, k, 0);
+       delay(4);     
     }
     
      // Fade OUT
     for(int k = 255; k > 0; k--) 
     { 
-       for (int i = 0; i < pixels; i++) 
-       {
-        strip.setPixelColor(i, 0, k, 0);
-       }
-         strip.show();
+        SetLedColour(0, k, 0);
         delay(4);     
     }
 }
 
 void IncomingCall()
 {
-  for (int i = 0; i < pixels; i++) 
-  {
-    strip.setPixelColor(i, 0, 0, 255);
-    strip.show();
+    SetLedColour(0, 0, 255);
     delay(1500);
-    strip.setPixelColor(i, 95, 255, 0);
-    strip.show();
+    SetLedColour( 95, 255, 0);
     delay(200);  
-  }
 }
 
 void SetLedColour(int G, int R, int B)
 {
- for (int i = 0; i < pixels; i++) 
-  {
-    strip.setPixelColor(i, G, R, B);
-  }
-  strip.show();
+	for (int i = 0; i < pixels; i++) 
+	{
+		strip.setPixelColor(i, R, G, B);
+	}
+	strip.show();
+ 
   //delay(5000); //Snooze for 5 secs so we're not spamming serial connection.
 }
 
@@ -140,4 +153,3 @@ void PartyMode()
     delay(50);
   }
 }
-
